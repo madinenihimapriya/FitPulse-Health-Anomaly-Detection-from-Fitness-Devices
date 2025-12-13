3e2758d93df898cef09ee8d7a6114b45527e8345
madinenihimapriya <madinenihimapriya8@gmail.com>
2025-12-13 18:44:42 +0530

    Remove app.py from Milestone1

--- Milestone1/app.py
+++
@@ -1,9 +0,0 @@
-from preprocess import load_and_preprocess
-
-def main():
-    data = load_and_preprocess("data")
-    print("Preprocessing completed successfully")
-    print(data.head())
-
-if __name__ == "__main__":
-    main()
